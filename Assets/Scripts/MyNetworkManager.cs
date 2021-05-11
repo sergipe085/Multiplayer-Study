6d55921a1e41ddbe1a63aa59c9310f9971baf6cb
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-05-10 21:03:57 -0300

    first commit

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        base.OnClientConnect(conn);
+
+        Debug.Log($"Connected to server {conn.address}");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        Debug.Log($"Player connected. Total: {numPlayers}");
+    }
+}
---
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SyncVar]
+    [SerializeField]
+    private string name = "None"
+}
