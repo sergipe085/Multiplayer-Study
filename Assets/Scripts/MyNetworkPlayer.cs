e6977c5c9fff0fd19617ed97d91946803da10b61
Sérgio <71034184+Cangaceiro15@users.noreply.github.com>
2021-05-11 07:18:13 -0300

    NEW: Command and ClientRpc

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Mirror;
 using TMPro;
@@ -13,26 +14,62 @@ public class MyNetworkPlayer : NetworkBehaviour
     [SerializeField]
     private string name = "None";
 
+    [SyncVar(hook = nameof(HandleColorUpdated))]
+    [SerializeField]
+    private Color color = Color.white;
+
+    #region Server
+
     [Server]
     public void SetName(string _name) {
         name = _name;
     }
 
-    private void HandleNameUpdated(string oldName, string newName) {
-        nameText.text = newName;
-    }
-
-    [SyncVar(hook=nameof(HandleColorUpdated))]
-    [SerializeField]
-    private Color color = Color.white;
-
     [Server]
     public void SetColor(Color _color) {
         color = _color;
-        GetComponentInChildren<MeshRenderer>().material.color = color;
+    }
+
+    [Command]
+    public void CmdSetName(string _name) {
+        string[] blackListed = new string[] { "Nigga" };
+        bool contain = false;
+        for (int i = 0; i < blackListed.Length; i++) {
+            if (_name.Contains(blackListed[i])) {
+                contain = true;
+                break;
+            }
+        }
+        
+        if (string.IsNullOrWhiteSpace(_name) || _name.Length >= 10 || contain) {
+            return;
+        }
+
+        RpcLogNewName(_name);
+        SetName(_name);
+    }
+
+    #endregion
+
+    #region Client
+
+    private void HandleNameUpdated(string oldName, string newName) {
+        nameText.text = newName;
     }
 
     private void HandleColorUpdated(Color oldColor, Color newColor) {
         renderer.material.color = newColor;
     }
+
+    [ContextMenu("Set My Name")]
+    private void SetMyName() {
+        CmdSetName("Nigga");
+    }
+
+    [ClientRpc]
+    private void RpcLogNewName(string _name) {
+        Debug.Log(_name);
+    }
+
+    #endregion
 }
